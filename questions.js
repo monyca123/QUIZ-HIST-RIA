035da4f904ceb0070d84e43e6c3c0d579c612fe6
monyca123 <monyca321.monaliza@gmail.com>
2024-12-31 01:50:26 -0300

    Quiz

---
+++ questions.js
@@ -0,0 +1,152 @@
+export default [
+  //01
+    {
+      question: "Com relação aos nomes dados ao município de Ibiara, marque a alternativa que corresponde aos dois nomes que ANTECEDERAM o nome IBIARA",
+      answers: [
+        { option: "Nossa Senhora do Rosário e Conceição", correct: false },
+        { option: "São Paulo e Poço do Cavalo", correct: false },
+        { option: "Poço do Cavalo e Santa Maria", correct: true },
+        { option: "Conceição e Santa Maria", correct: false },
+      ],
+    },
+    //02
+    {
+      question: "Marque a alternativa que explique o significado do nome IBIARA",
+      answers: [
+        { option: "“Senhores da Terra” e “Terra que tem dono”", correct: true },
+        { option: "“Imensa floresta”", correct: false },
+        { option: "“Pedra bonita”", correct: false },
+        { option: "“Rio ruim para navegar”", correct: false },
+      ],
+    },
+    //03
+    {
+      question: "Além dos símbolos do município, as escolas municipais também possuem hino e escudo. Marque a alternativa que corresponde o(a) autor(a) do hino escolar e municipal",
+      answers: [
+        { option: "Maria Elenilda Simão", correct: false },
+        { option: "Antonia Giseuda Leite de Moura", correct: true },
+        { option: "Francisco Nenivaldo de Sousa", correct: false },
+        { option: "Lucineide Pereira", correct: false },
+      ],
+    },
+    //04
+    {
+      question: "A data, mês e ano que marcam a emancipação política de Ibiara, respectivamente, é:",
+      answers: [
+        { option: "16 de maio de 1959", correct: false },
+        { option: "17 de abril de 1959", correct: true },
+        { option: "17 de abril de 1960", correct: false },
+        { option: "19 de abril de 1959", correct: false },
+      ],
+    },
+    //05
+    {
+      question: "As cidades limítrofes do município de Ibiara são",
+      answers: [
+        { option: "Bonito de Santa Fé, Conceição, Diamante", correct: false },
+        { option: "Conceição, Itaporanga, Mata Grande", correct: false },
+        { option: "Santana de Mangueira, Diamante, Conceição", correct: true },
+        { option: "Santana de Mangueira, Santa Inês, Diamante", correct: false },
+      ],
+    },
+    //06
+    {
+      question: " A primeira escola construída no município de Ibiara foi denominada de:",
+      answers: [
+        { option: "Joaquim Lopes Ribeiro", correct: false },
+        { option: "Ceci Badú de Sousa", correct: false },
+        { option: "França Leite", correct: true },
+        { option: "Alice Leite", correct: false },
+      ],
+    },
+    //07
+    {
+      question: "Em novembro de 1959 aconteceu um evento importante no município que contou com a participação da população, sendo este:",
+      answers: [
+        { option: "primeira eleição municipal", correct: true },
+        { option: "inauguração da primeira escola", correct: false },
+        { option: "inauguração da casa de saúde", correct: false },
+        { option: "inauguração da prefeitura", correct: false },
+      ],
+    },
+    //08
+    {
+      question: "Filho ilustre de Ibiara responsável por designar o nome Ibiara ao município:",
+      answers: [
+        { option: "Padre Cícero Romão Batista", correct: false },
+        { option: "Padre Inácio de Sousa Rolim", correct: false },
+        { option: "Padre Manoel Otaviano de Moura Lima", correct: true },
+        { option: "Padre João Andriola", correct: false },
+      ],
+    },
+    //09
+    {
+      question: "Na década de 1980, a primeira mulher assumiu o cargo de prefeita de Ibiara, sendo ela:",
+      answers: [
+        { option: "Maria Ivonete Vieira Rodrigues", correct: true },
+        { option: "Margarida Ramalho", correct: false },
+        { option: "Maria Lopes Ribeiro", correct: false },
+        { option: "Estelina Ramalho", correct: false },
+      ],
+    },
+    //10
+    {
+      question: "Com um sistema de saúde ainda incipiente, na década de 1940 e 1950, um cidadão do município desempenhou um grande trabalho como parteiro, farmacêutico, dentista, conhecido por:",
+      answers: [
+        { option: "Narciso Arruda", correct: false },
+        { option: "José De Góes", correct: false },
+        { option: "Isaías Rodrigues ou “Sinhô Rodrigues”", correct: true },
+        { option: "Severino", correct: false },
+      ],
+    },
+    //11
+    {
+      question: "Dois rios cortam o território ibiarense, são eles:",
+      answers: [
+        { option: "Rio Piranhas e Rio Grande", correct: false },
+        { option: "Rio Piancó e Rio Santa Maria", correct: true },
+        { option: "Rio do Peixe e Rio Grande", correct: false },
+        { option: "Rio da Malhada e Rio da Pedra", correct: false },
+      ],
+    },
+    //12
+    {
+      question: "Vilas e povoados pertencentes ao município de Ibiara:",
+      answers: [
+        { option: "Sítio Alves, Serra Vermelha e Vazante", correct: false },
+        { option: "Vazante, Ibiarinha e Quintiliano", correct: false },
+        { option: "Cachoeirinha, Ibiarinha e Vazante", correct: false },
+        { option: "Cachoeirinha, Várzea Redonda", correct: true },
+      ],
+    },
+    //13
+    {
+      question: "No processo de emancipação política, um prefeito interino assumiu o cargo até a data das eleições, foi ele:",
+      answers: [
+        { option: "Antônio Ramalho Diniz", correct: true },
+        { option: "Afonso Kerhle", correct: false },
+        { option: "Osório Pinto Ramalho", correct: false },
+        { option: "Jesus Ramalho", correct: false },
+      ],
+    },
+    //14
+    {
+      question: "Geograficamente falando, a qual mesorregião paraibana pertence o município de Ibiara?",
+      answers: [
+        { option: "Mesorregião do Sertão Paraibano", correct: true },
+        { option: "Mesorregião da Borborema", correct: false },
+        { option: "Mesorregião do Agreste Paraibano", correct: false },
+        { option: "Mesorregião da Mata Paraibana", correct: false },
+      ],
+    },
+    //15
+    {
+      question: "Duas das principais fontes de renda do município de Ibiara, são:",
+      answers: [
+        { option: "Indústria e vendas", correct: false },
+        { option: "Indústria e pecuária", correct: false },
+        { option: "Agricultura e pecuária", correct: true },
+        { option: "Vendas e agricultura", correct: false },
+      ],
+    },
+  ];
